8938961b50559fa349b16a597a624a940cbdfd98
Beardad23 <beardad23@gmail.com>
2024-07-25 00:53:25 +0200

    numb3rs

--- cs50 /python/assignments/numb3rs/numb3rs.py
+++ cs50 /python/assignments/numb3rs/numb3rs.py
@@ -11,17 +11,17 @@ def validate(ip):
         a, b, c, d = ip.split(".")
         ip_list = [a, b, c, d]
 
-        while True:
-            for number in ip_list:
-                if int(number) < 0 or int(number) > 255:
-                    return False
+    try:  
+        for number in ip_list:
+            if int(number) < 0 or int(number) > 255:
+                return False
 
             else:
                 return True
-                    
     else:
         return False
-
+    except ValueError:
+        sys.exit("ValueError")
 
 if __name__ == "__main__":
     main()
